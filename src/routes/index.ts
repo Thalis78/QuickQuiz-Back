0e50de90f3c3ffef890d3af2426049e457b47da5
Lucas Morais <lukazmorayz@gmail.com>
2026-05-07 22:06:35 -0300

    :sparkles: feat: Adiciona rota de /sala/criar

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware";
 import * as authController from "../controllers/authController";
+import * as roomController from "../controllers/roomController";
 
 const router = Router();
 
 router.post("/login", authController.loginProfessor);
+
+router.post("/sala/criar", authMiddleware, roomController.criarSala);
 
 export default router;
